5520540fd16e0f31b3b1e271e476a7ece6a86c82
Chaz Simons <chaz.simons@gmail.com>
2021-11-12 12:57:16 -0800

    Add model test for doctor count method

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Hospital do
+  before :each do
+    @gh = Hospital.create!({name: 'General Hospital'})
+    @scrubs = Hospital.create({name: 'Scrubs Hospital'})
+    @childrens = Hospital.create({name: 'Childrens Hospital'})
+
+    @doc_1 = Doctor.create!({name: 'Donovan Doctor', specialty: 'General Practice', university: 'Stanford Law', hospital_id: @gh.id})
+    @doc_2 = Doctor.create!({name: 'Greg House', specialty: 'Crabby Specialist', university: 'Oxford', hospital_id: @gh.id})
+    @doc_3 = Doctor.create!({name: 'Sandra Who', specialty: 'Time Surgery', university: 'TARDIS University', hospital_id: @scrubs.id})
+    @doc_4 = Doctor.create!({name: 'Lola Spratt', specialty: 'Pediactric Surgery', university: 'Clown College School of Medicine', hospital_id: @childrens.id})
+  end
+
   describe 'relationships' do
     it { should have_many(:doctors) }
+  end
+
+  it 'can return a count of doctors at a give hospital' do
+    expect(@gh.doc_count).to eq(2)
+    expect(@scrubs.doc_count).to eq(1)
+    expect(@childrens.doc_count).to eq(1)
   end
 end
